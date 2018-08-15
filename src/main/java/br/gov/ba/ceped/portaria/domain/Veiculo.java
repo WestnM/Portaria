4296b832161cc00bb9e3a13ff4adf2975115ebf2
wsouza <wsouza@CEPED-GERIM05.ceped.ba.gov.br>
2018-08-15 11:22:02 -0300

    Classe veiculo

--- src/main/java/br/gov/ba/ceped/portaria/domain/Veiculo.java
+++ src/main/java/br/gov/ba/ceped/portaria/domain/Veiculo.java
@@ -1,5 +1,70 @@
 package br.gov.ba.ceped.portaria.domain;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_veiculo")
 public class Veiculo {
+	
+		@Column(name = "veic_marca", length = 40, nullable = false)
+		private String marca;
+		
+		@Column(name = "veic_modelo", length = 20, nullable = false)
+		private String modelo;
+
+		@Column(name = "veic_placa", nullable = false, length = 7, unique = true)
+		private String placa;
+
+		@Column(name = "veic_empresa", nullable = false)
+		private String empresa;
+		
+		@Column(name = "veic_tipo")
+		private Enum Tipo;
+
+		public String getMarca() {
+			return marca;
+		}
+
+		public void setMarca(String marca) {
+			this.marca = marca;
+		}
+
+		public String getModelo() {
+			return modelo;
+		}
+
+		public void setModelo(String modelo) {
+			this.modelo = modelo;
+		}
+
+		public String getPlaca() {
+			return placa;
+		}
+
+		public void setPlaca(String placa) {
+			this.placa = placa;
+		}
+
+		public String getEmpresa() {
+			return empresa;
+		}
+
+		public void setEmpresa(String empresa) {
+			this.empresa = empresa;
+		}
+
+		public Enum getTipo() {
+			return Tipo;
+		}
+
+		public void setTipo(Enum tipo) {
+			Tipo = tipo;
+		}
+		
+		
 
 }
