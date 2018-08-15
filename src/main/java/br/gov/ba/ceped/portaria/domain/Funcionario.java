46bf013b9d95df7b2b10a1b5239ecd3275de9d28
wsouza <wsouza@CEPED-GERIM05.ceped.ba.gov.br>
2018-08-15 11:04:24 -0300

    Commit Teste

---
+++ src/main/java/br/gov/ba/ceped/portaria/domain/Funcionario.java
@@ -0,0 +1,58 @@
+package br.gov.ba.ceped.portaria.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_funcionarios")
+public class Funcionario extends GenericDomain {
+	
+	@Column(name = "fun_nome", length = 40, nullable = false)
+	private String nome;
+
+	@Column(name = "fun_cpf", nullable = false, unique = true)
+	private String cpf;
+	
+	@Column(name = "fun_setor", length = 40, nullable = false)
+	private String setor;
+
+	@Column(name = "fun_telefone")
+	private String telefone;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+
+
+}
