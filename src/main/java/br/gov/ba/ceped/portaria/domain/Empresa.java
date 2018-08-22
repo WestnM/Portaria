86b6504e33a40d4d1034368a5a758f31baa1f009
wsouza <wsouza@CEPED-GERIM05.ceped.ba.gov.br>
2018-08-22 14:00:11 -0300

    Empresa e Veiculo!

--- src/main/java/br/gov/ba/ceped/portaria/domain/Empresa.java
+++ src/main/java/br/gov/ba/ceped/portaria/domain/Empresa.java
@@ -2,13 +2,30 @@ package br.gov.ba.ceped.portaria.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table (name = "tbl_empresa")
 public class Empresa{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long codigo;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
 
 	@Column(length = 50)
 	private String nomeFantasia;
+	
 	@Column(length = 30)
 	private String cnpj;
 
@@ -20,8 +37,7 @@ public class Empresa{
 	public void setNomeFantasia(String nomeFantasia) {
 		this.nomeFantasia = nomeFantasia;
 	}
-
-	@Id
+	
 	public String getCnpj() {
 		return cnpj;
 	}
--- src/main/java/br/gov/ba/ceped/portaria/domain/Veiculo.java
+++ src/main/java/br/gov/ba/ceped/portaria/domain/Veiculo.java
@@ -1,11 +1,15 @@
 package br.gov.ba.ceped.portaria.domain;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "tbl_veiculo")
@@ -28,6 +32,18 @@ public class Veiculo {
 	@Column(name = "veic_km_entrada")
 	private String km;
 
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date veic_data;
+	
+	public Date getData() {
+		return veic_data;
+	}
+
+	public void setData(Date data) {
+	this.veic_data = data;
+	}
+	
 	public String getMarca() {
 		return marca;
 	}
