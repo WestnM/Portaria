fadb69a839bdab97375f460c39bef279ff0e037f
wsouza <wsouza@CEPED-GERIM05.ceped.ba.gov.br>
2018-08-22 14:40:15 -0300

    .

--- src/main/java/br/gov/ba/ceped/portaria/domain/Pessoa.java
+++ src/main/java/br/gov/ba/ceped/portaria/domain/Pessoa.java
@@ -1,20 +1,39 @@
 package br.gov.ba.ceped.portaria.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @SuppressWarnings("serial")
 @MappedSuperclass
 public class Pessoa implements Serializable {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long codigo;
+
+	@Column(name = "pes_nome", length = 40, nullable = false)
+	private String nome;
+
+	@Column(name = "pes_cpf", length = 14, unique = true)
+	private String cpf;
+
+	@Column(name = "pes_rg", length = 14, unique = true)
+	private String rg;
+
+	@Column(name = "pes_telefone")
+	private String telefone;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date pes_data;
 
 	public Long getCodigo() {
 		return codigo;
@@ -23,18 +42,6 @@ public class Pessoa implements Serializable {
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-	@Column(name = "pes_nome", length = 40, nullable = false)
-	private String nome;
-	
-	@Column(name = "pes_cpf",length = 14, unique = true)
-	private String cpf;
-	
-	@Column(name = "pes_rg",length = 14, unique = true)
-	private String rg;
-
-	@Column(name = "pes_telefone")
-	private String telefone;
-
 
 	public String getRg() {
 		return rg;
@@ -68,5 +75,11 @@ public class Pessoa implements Serializable {
 		this.telefone = telefone;
 	}
 
-}
+	public Date getData() {
+		return pes_data;
+	}
 
+	public void setData(Date data) {
+		this.pes_data = data;
+	}
+}
--- src/main/java/br/gov/ba/ceped/portaria/domain/Visitante.java
+++ src/main/java/br/gov/ba/ceped/portaria/domain/Visitante.java
@@ -1,6 +1,7 @@
 package br.gov.ba.ceped.portaria.domain;
 
 import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -9,8 +10,15 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tbl_visitante")
 public class Visitante extends Pessoa implements Serializable {
-
+	
 	@Column(name = "vis_empresa", length = 40, nullable = false)
 	private String empresa;
 
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
 }
