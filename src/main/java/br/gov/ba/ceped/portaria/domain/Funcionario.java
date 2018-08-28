0f04171a2b993c277867741ce79db11574fb37ad
wsouza <wsouza@CEPED-GERIM05.ceped.ba.gov.br>
2018-08-28 09:18:41 -0300

    ..

--- src/main/java/br/gov/ba/ceped/portaria/domain/Funcionario.java
+++ src/main/java/br/gov/ba/ceped/portaria/domain/Funcionario.java
@@ -1,22 +1,39 @@
 package br.gov.ba.ceped.portaria.domain;
+
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "tbl_funcionarios")
-public class Funcionario extends Pessoa {
+public class Funcionario extends Pessoa implements Serializable {
 
+	@Id
 	@Column(name = "fun_matric", length = 40, nullable = false)
 	private String matricula;
 
 	@Column(name = "fun_setor", length = 40, nullable = false)
 	private String setor;
-	
+
 	@Column(name = "fun_cargo", length = 40, nullable = false)
 	private String cargo;
 	
+	@JoinColumn(nullable = false)
+	private Pessoa pessoa;
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
 	public String getCargo() {
 		return cargo;
 	}
